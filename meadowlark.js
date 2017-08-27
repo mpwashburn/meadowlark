820410a7e38f89e73ef36958107912d84d20d996
Michael Washburn <washburn.michael@gmail.com>
2017-08-26 19:48:20 -0700

    added partials from chapter 7

--- meadowlark.js
+++ meadowlark.js
@@ -1,20 +1,68 @@
 var express =  require('express');
+var fortune = require('./lib/fortune.js');
+
 var app = express();
-var handlebars = require('express-handlebars').create({ defaultLayout:'main'});
+
+// set up handlebars view engine
+var handlebars = require('express-handlebars').create({
+    defaultLayout:'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+  });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
-var fortune = require('./lib/fortune.js');
+
 // STATIC MIDDLEWARE
 app.use(express.static(__dirname + '/public'));
 
 
 // *******************************************************************
 // TESTING MIDDLEWARE
-
+// set 'showTests' context property if the querystring contains test=1
 app.use(function(req, res, next){
         res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
+        next();
+});
+
+// mocked weather data
+function getWeatherData(){
+    return {
+      locations: [
+          {
+              name: 'Portland',
+              forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+              iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+              weather: 'Overcast',
+              temp: '54.1 F (12.3 C)',
+          },
+          {
+              name: 'Bend',
+              forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+              iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+              weather: 'Partly Cloudy',
+              temp: '55.0 F (12.8 C)',
+          },
+          {
+              name: 'Manzanita',
+              forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+              iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+              weather: 'Light Rain',
+              temp: '55.0 F (12.8 C)',
+          },
+      ],
+    };
+}
+// middleware to add weather data to context
+app.use(function(req, res, next){
+        if(!res.locals.partials) res.locals.partials = {};
+        res.locals.partials.weatherContext = getWeatherData();
         next();
 });
 
@@ -51,6 +99,7 @@ app.use(function(err, req, res, next){
         res.status(500);
         res.render('500');
 });
+
 
 app.listen(app.get('port'), function(){
   console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
