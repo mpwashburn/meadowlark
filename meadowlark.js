eda71f30439e783749dcda7730f9a9aaa88b3f24
Michael Washburn <washburn.michael@gmail.com>
2017-08-27 18:57:06 -0700

    finished chapter 8 and form handling

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,5 @@
 var express =  require('express');
 var fortune = require('./lib/fortune.js');
-var express_handlebars_sections = require('express-handlebars-sections');
 
 var app = express();
 
@@ -17,10 +16,14 @@ var handlebars = require('express-handlebars').create({
   });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
 // STATIC MIDDLEWARE
 app.use(express.static(__dirname + '/public'));
+// app.use(require('body-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+
 
 
 // *******************************************************************
@@ -104,6 +107,36 @@ app.get('/data/nursery-rhyme', function(req, res){
                   noun: 'heck'
         });
 });
+
+// app.get('/thank-you', function(req, res){
+//         res.render('thank-you');
+
+app.get('/newsletter', function(req, res){
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+app.post('/process', function(req, res){
+    if(req.xhr || req.accepts('json,html')==='json'){
+      console.log('Form (from querystring): ' + req.query.form);
+        console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+        console.log('Name (from visible form field): ' + req.body.name);
+        console.log('Email (from visisble form field): ' + req.body.email);
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
+});
+// app.post('/process', function(req, res){
+//         console.log('Form (from querystring): ' + req.query.form);
+//         console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+//         console.log('Name (from visible form field): ' + req.body.name);
+//         console.log('Email (from visisble form field): ' + req.body.email);
+//         res.redirect(303, '/thank-you');
+// });
 
 // custom 404 page
 app.use(function(req, res){
