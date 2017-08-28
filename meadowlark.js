efc6cad3251c2eef75160588e12a76af4f60c4b5
Michael Washburn <washburn.michael@gmail.com>
2017-08-28 15:47:02 -0700

    moving to ch9 added credentials.js

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 var express =  require('express');
 var fortune = require('./lib/fortune.js');
 var formidable = require('formidable');
+var credentials = require('./credentials.js');
 
 var app = express();
 
@@ -112,6 +113,18 @@ app.get('/contest/vacation-photo', function(req, res){
         var now = new Date();
         res.render('contest/vacation-photo',{
             year: now.getFullYear(),month: now.getMonth()
+        });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res) {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files){
+            if(err) return res.redirect(303, '/error');
+            console.log('received fields:');
+            console.log(fields);
+            console.log('received files:');
+            console.log(files);
+            res.redirect(303, '/thank-you');
         });
 });
 // app.get('/thank-you', function(req, res){
